728e782ad93bae29452bca26554a5f9f8eced680
socar-ornn <ornn@socar.kr>
2022-06-11 11:32:12 +0900

    init the project

---
+++ src/main/java/com/pagination/paginationExample/PaginationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.pagination.paginationExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pagination/paginationExample/PaginationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagination.paginationExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
